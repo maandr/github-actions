c28894cf4d66dd585ceee7f0fd2e7671ab5fff30
Mats Andresen <mats.andresen@gmx.de>
2023-04-20 21:53:39 +0200

    ci-go: Add ci-go reusable-workflow

---
+++ tests/go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func sum(a int32, b int32) int32 {
+	return a + b
+}
---
+++ tests/go/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	result := sum(1, 2)
+	if result != 3 {
+		t.Errorf("sum(1, 2) = %d expected 3", result)
+	}
+}
